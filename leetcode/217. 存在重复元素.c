4ae6d9b3af83777819e8c77da4ee1841042d7042
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-04-10 23:59:00 +0800

    Add files via upload

---
+++ leetcode/217. 存在重复元素.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int cmp(const void* _a, const void* _b) {
+	int a = *(int*)_a, b = *(int*)_b;
+	return a - b;
+}
+
+for (int i = 0; i < numsSize - 1; i++) {
+	if (nums[i] == nums[i + 1]) {
+		return true;
+	}
+}
+}
+
+
+
+int main()
+{
+
+	return;
+}
