fdcf86b45c7976dd5c50737fdce39445ac0e15cc
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-04-11 09:02:16 +0800

    Update 217. 存在重复元素.c

--- leetcode/217. 存在重复元素.c
+++ leetcode/217. 存在重复元素.c
@@ -1,21 +1,29 @@
-#include <stdio.h>
-
 int cmp(const void* _a, const void* _b) {
-	int a = *(int*)_a, b = *(int*)_b;
-	return a - b;
+    int a = *(int*)_a, b = *(int*)_b;
+    return a - b;
 }
 
-for (int i = 0; i < numsSize - 1; i++) {
-	if (nums[i] == nums[i + 1]) {
-		return true;
-	}
-}
-}
+//先排序，再比较。如果两个相邻元素重复，让计数器增加。如果计数器不等于初值，就返回true。
+bool containsDuplicate(int* nums, int numsSize){
 
+    qsort(nums, numsSize, sizeof(int), cmp);
+    
+    int count = 0;
 
+    int i;
+    for(i=0;i<numsSize-1;i++)
+    {   
+        if(count == 1)
+            return true;
 
-int main()
-{
+        if(nums[i] == numjis[i+1])
+        {
+            count++;
+        }
+    }
 
-	return;
+     if(count == 1)
+            return true;
+
+    return false;
 }
