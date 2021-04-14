195f2ce08a3cdcba9ce3641890a6b235bbffa745
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-04-14 09:35:40 +0800

    Add files via upload

---
+++ leetcode/剑指 Offer 04. 二维数组中的查找.c
@@ -0,0 +1,26 @@
+
+
+//—Ó œæÿ’Ûµƒ≤È’“
+bool findNumberIn2DArray(int** matrix, int matrixSize, int* matrixColSize, int target) {
+
+
+	int row = 0;
+	int col = *matrixColSize - 1;
+
+	if (matrixSize == 0 || *matrixColSize == 0)
+		return false;
+
+	while (row < matrixSize && col >= 0) {
+
+		if (target > matrix[row][col]) {
+			row++;
+		}
+		else if (target < matrix[row][col]) {
+			col--;
+		}
+		else if (target == matrix[row][col]) {
+			return true;
+		}
+	}
+	return false;
+}
