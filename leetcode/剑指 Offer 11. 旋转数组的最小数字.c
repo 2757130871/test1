fb3fe59a53c9da40c270e1567c971318e9715591
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-04-15 22:38:27 +0800

    Update 剑指 Offer 11. 旋转数组的最小数字.c

--- leetcode/剑指 Offer 11. 旋转数组的最小数字.c
+++ leetcode/剑指 Offer 11. 旋转数组的最小数字.c
@@ -1,7 +1,36 @@
-#include <stdio.h>
 
-int main()
-{
 
-	return;
+//利用二分查找
+
+int findMin(int* nums, int numsSize) {
+
+	int low = 0;
+	int high = numsSize - 1;
+
+	//当第一个元素小于最后元素说明有序，直接返回
+	//数组只有一个元素时直接返回
+	if (nums[low] < nums[high] || low == high)
+	{
+		return nums[low];
+	}
+
+	//
+	while (low + 1 != high) {
+
+		int pivot = low + (high - low) / 2;
+		if (nums[pivot] < nums[high]) {
+			high = pivot;
+		}
+		else if (nums[pivot] > nums[high]) {
+			low = pivot;
+		}
+		else
+		{
+			high -= 1;
+		}
+	}
+
+	return nums[high];
 }
+
+
