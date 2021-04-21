b2b5b942207f92eb2ce54327f7a24cb0934b0bae
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-04-21 22:29:10 +0800

    Update 33. 搜索旋转排序数组.c

--- leetcode/33. 搜索旋转排序数组.c
+++ leetcode/33. 搜索旋转排序数组.c
@@ -1,8 +1,44 @@
-#include <stdio.h>
+//思路：
+// nums[mid]小于nums[right] 说明右子数组是有序的，
+// nums[mid]大于nums[left] 说明左子数组是有序的，
+//通过判断target在哪边 可以判断出保留哪边
 
+int search(int* nums, int numsSize, int target) {
 
-int main()
-{
+	int left = 0;
+	int right = numsSize - 1;
 
-	return;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+
+		if (nums[mid] == target)
+			return mid;
+
+		if (nums[mid] < nums[right])
+		{
+			if (nums[mid] < target && target <= nums[right])// 
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid - 1;
+			}
+		}
+		else
+		{
+			if (nums[mid] > target && nums[left] <= target)//
+			{
+				right = mid - 1;
+			}
+			else
+			{
+				left = mid + 1;
+			}
+		}
+
+	}
+
+	return -1;
 }
