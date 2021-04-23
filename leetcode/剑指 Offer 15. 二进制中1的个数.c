d365bc50d0b744ceb3774f90d631c43f811b8be2
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-04-23 17:45:53 +0800

    Update 剑指 Offer 15. 二进制中1的个数.c

--- leetcode/剑指 Offer 15. 二进制中1的个数.c
+++ leetcode/剑指 Offer 15. 二进制中1的个数.c
@@ -1,7 +1,33 @@
-#include <stdio.h>
+//方法1：让n的二进制每一位和1进行与运算，
+//如果为1就让计数器增加
+int hammingWeight(unsigned int n) {
 
-int main()
-{
+	unsigned int flag = 1;
+	int count = 0;
+	while (flag)
+	{
+		if (n & flag)
+		{
+			count++;
+		}
+		flag <<= 1;
+	}
+	return count;
+}
 
-	return;
+
+//方法2：
+//让一个非0的数和这个数-1进行&运算，就会让这个数的二进制位少一个1、
+// 1100 & 1011 = 1000 
+//利用这个规律解决
+
+int hammingWeight(uint32_t n) {
+
+    int count = 0;
+    while(n)
+    {
+        count++;
+        n = n & (n - 1);
+    }
+    return count;
 }
