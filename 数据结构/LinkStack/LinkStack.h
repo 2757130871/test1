20ea17cf25119809831c14f8748bf4e8d6612b53
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-05-03 23:49:10 +0800

    Add files via upload

---
+++ 数据结构/LinkStack/LinkStack.h
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+#define OK 1
+#define ERROR -1
+
+typedef int Status;
+
+struct LinkStack
+{
+	struct LinkStack* top;
+	int count;
+};
+
+
+//Create LinkStack
+
+
+//Init LinkStack
+
