10528127dde5838b346c066c823cfc2d76ec885b
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-05-04 00:43:15 +0800

    Update LinkStack.h

--- 数据结构/LinkStack/LinkStack.h
+++ 数据结构/LinkStack/LinkStack.h
@@ -6,16 +6,35 @@
 #define ERROR -1
 
 typedef int Status;
+typedef int ElemType;
+
+struct StackNode
+{
+	ElemType data;
+	struct StackNode* next;
+};
 
 struct LinkStack
 {
-	struct LinkStack* top;
+	struct StackNode* top;
 	int count;
 };
 
 
-//Create LinkStack
 
+//Create LinkStack
+struct LinkStack* CreateLinkStack();
 
 //Init LinkStack
+void InitLinkStack(struct LinkStack* L);
+
+//Print LinkStack
+void PrintLinkStack(struct LinkStack* L);
+
+//Push
+void PushLinkStack(struct LinkStack* L, int e);
+
+//Pop
+void PopLinkStack(struct LinkStack* L, int* e);
+
 
