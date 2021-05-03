7ecdbd7f9fcb5d40192c94861ad06281b9447bb0
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-05-04 00:43:38 +0800

    Update test.c

--- 数据结构/LinkStack/test.c
+++ 数据结构/LinkStack/test.c
@@ -5,7 +5,32 @@
 
 int main()
 {
-	struct LinkStack l1;
+	struct LinkStack* L1 = CreateLinkStack();
+
+	//Init
+	InitLinkStack(L1);
+
+	//Push
+	PushLinkStack(L1, 1);
+	PushLinkStack(L1, 2);
+	PushLinkStack(L1, 3);
+	PushLinkStack(L1, 4);
+
+	//Print
+ 	PrintLinkStack(L1);
+
+	printf("\n");
+
+	//Pop
+	int e;
+	PopLinkStack(L1, &e);
+	PopLinkStack(L1, &e);
+	PopLinkStack(L1, &e);
+	PopLinkStack(L1, &e);
+
+	//Print
+	PrintLinkStack(L1);
 
 	return 0;
 }
+
