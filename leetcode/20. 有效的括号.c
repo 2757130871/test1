a4b29698904eb64dae7844edb47038ddd5ad2884
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-05-07 23:56:26 +0800

    Add files via upload

---
+++ leetcode/20. 有效的括号.c
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        unordered_map<char, char> pairs = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        stack<char> stk;
+        for (char ch : s) {
+            if (pairs.count(ch)) {
+                if (stk.empty() || stk.top() != pairs[ch]) {
+                    return false;
+                }
+                stk.pop();
+            }
+            else {
+                stk.push(ch);
+            }
+        }
+        return stk.empty();
+    }
+};
+复杂度分析
+
+时间复杂度：O(n)O(n)，其中 nn 是字符串 ss 的长度。
