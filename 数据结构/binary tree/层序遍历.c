e0a4d6ab9798b0a44faade7f51b00bb1def07ef2
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-05-11 23:59:24 +0800

    Update 层序遍历.c

--- 数据结构/binary tree/层序遍历.c
+++ 数据结构/binary tree/层序遍历.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
-
-int main()
+//层序遍历
+void postorder(treeNode* Node)
 {
+    Tnode* Queue[1000]; //创建队列
+    int front = -1,rear = -1;
 
-	return;
+    Tnode* p;
+	
+    Queue[++rear] = root;
+    while(front < rear)
+    {
+        p = Queue[++front]; 
+        printf("%d ",p->val);
+        if(p->left) 
+            Queue[++rear] = p->left;
+        if(p->right)
+            Queue[++rear] = p->right;
+            
+    }
 }
