2b80d6c0dc2288bf8153fc32c459bf40b568b098
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-07-23 19:29:42 +0800

    Update contact.h

--- 日常练习题/contact/contact.h
+++ 日常练习题/contact/contact.h
@@ -1,16 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAX 1000
+#define MAX_NAME 20
+#define MAX_ADDR 20
+#define MAX_TELE 11
 
 typedef struct PepInfo
 {
-	char name[20];
-	int number;
-
+	char name[MAX_NAME];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
 }PepInfo;
 
- 
+
 typedef struct contact
 {
-	PepInfo data[1000];
+	PepInfo data[MAX];
 	int sz;
+
 }contact;
+
+void InitContact(contact* con);
+void AddContact(contact* con);
+void DeleteContact(contact* con);
+void ModifyContact(contact* con);
+void SearchContact(contact* con);
+void PrintContact(contact* con);
 
