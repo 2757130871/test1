dda2d254110152c85527256bc4e10ca90e1dd51e
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-07-27 23:56:49 +0800

    Add files via upload

---
+++ 日常练习题/SqList/SqList.c
@@ -0,0 +1,39 @@
+﻿#include "SqList.h"
+
+
+void SqListInit(SqList* list)
+{
+	assert(list);
+
+	list->capasity = 4;
+	list->data = (SqList*)malloc(list->capasity * sizeof(SqDataType));
+	list->sz = 0;
+
+}
+
+void SqListDestroy(SqList* list)
+{
+	assert(list);
+	
+	free(list);
+	list = NULL;
+}
+
+
+void SqlistPushBack(SqList* list, SqDataType val)
+{
+	assert(list);
+
+	if (list->sz == list->capasity)
+	{
+		//增容
+
+	}
+
+}
+
+void SqlistPushFront(SqList* list, SqDataType val)
+{
+
+
+}
