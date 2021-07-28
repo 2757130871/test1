03a230b8ac6568f1935a78ae87effd3d3aee3f0c
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-07-28 21:24:13 +0800

    Update SqList.c

--- 日常练习题/SqList/SqList.c
+++ 日常练习题/SqList/SqList.c
@@ -1,39 +1,155 @@
-﻿#include "SqList.h"
+#include "SqList.h"
 
-
+//	初始化
 void SqListInit(SqList* list)
 {
 	assert(list);
 
-	list->capasity = 4;
-	list->data = (SqList*)malloc(list->capasity * sizeof(SqDataType));
+	list->capacity = 4;
+	list->data = (SqDataType*)malloc(list->capacity * sizeof(SqDataType));
 	list->sz = 0;
-
 }
 
-void SqListDestroy(SqList* list)
+//	销毁顺序表
+void SqListDestroy(SqDataType* ptr)
 {
-	assert(list);
-	
-	free(list);
-	list = NULL;
+	assert(ptr);
+
+	free(ptr);
+	ptr = NULL;
 }
 
-
-void SqlistPushBack(SqList* list, SqDataType val)
+void CheckCapacity(SqList* list)
 {
 	assert(list);
 
-	if (list->sz == list->capasity)
+	if (list->sz == list->capacity)
 	{
 		//增容
+		SqDataType* newList =
+			(SqDataType*)realloc(list->data, (list->capacity + INCREMENT) * sizeof(SqDataType));
 
+		if (newList)
+			list->data = newList;
+
+		list->capacity += INCREMENT;
 	}
-
 }
 
-void SqlistPushFront(SqList* list, SqDataType val)
+//	尾插
+void SqListPushBack(SqList* list, SqDataType val)
 {
+	assert(list);
+
+	CheckCapacity(list);
+
+	list->data[list->sz] = val;
+	list->sz++;
+}
+
+//	头插
+void SqListPushFront(SqList* list, SqDataType val)
+{
+	assert(list);
+
+	CheckCapacity(list);
+
+	int i;
+	for (i = list->sz - 1; i >= 0; i--)
+	{
+		list->data[i + 1] = list->data[i];
+	}
 
 
+	list->data[0] = val;
+	list->sz++;
+}
+
+//	尾删
+void SqListPopBack(SqList* list, SqDataType* val)
+{
+	assert(list);
+	assert(list->sz > 0);
+	*val = list->data[list->sz - 1];
+	list->sz--;
+}
+
+
+//	头删
+
+void SqListPopFront(SqList* list, SqDataType* val)
+{
+	assert(list);
+	assert(list->sz > 0);
+
+	*val = list->data[0];
+
+	int n = 0;
+	while (n < list->sz - 1)
+	{
+		list->data[n] = list->data[n + 1];
+		n++;
+	}
+
+	list->sz--;
+}
+
+
+//	插入
+void SqListInsert(SqList* list, size_t pos, SqDataType val)
+{
+	assert(list);
+	assert((int)pos >= 0 && (int)pos <= list->sz);
+
+	CheckCapacity(list);
+
+
+	int i = list->sz - 1;
+	while (i >= ((int)pos))
+	{
+		list->data[i + 1] = list->data[i];
+		--i;
+	}
+
+	list->data[pos] = val;
+	list->sz++;
+}
+
+
+//	删除
+void SqListErase(SqList* list, size_t pos, SqDataType* val)
+{
+	assert(list);
+	assert(list->sz != 0);
+	assert(((int)pos) >= 0 && ((int)pos) < list->sz);
+
+
+	*val = list->data[pos];
+	if (list->sz == 1)
+	{
+		list->sz--;
+		return;
+	}
+
+	int i = pos;
+	while (i < (list->sz - 1))
+	{
+		list->data[i] = list->data[i + 1];
+		i++;
+	}
+
+	list->sz--;
+}
+
+
+
+
+//	打印
+void SqListPrint(SqList* list)
+{
+	int i;
+	for (i = 0; i < list->sz; i++)
+	{
+		printf("%d\n", list->data[i]);
+	}
 }
