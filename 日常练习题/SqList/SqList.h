95a2385563f04dbcf9541cdd9873d5c2ab988768
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-07-28 21:24:31 +0800

    Update SqList.h

--- 日常练习题/SqList/SqList.h
+++ 日常练习题/SqList/SqList.h
@@ -1,28 +1,30 @@
 #pragma once
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
 typedef  int SqDataType;
-#define INCREMENT 2
+#define INCREMENT 10
 
 
 typedef struct SqList {
 
 	SqDataType* data;
 	int sz;
-	int capasity;
+	int capacity;
 
 }SqList;
 
-
 void SqListInit(SqList* list);
-void SqListDestroy(SqList* list);
+void SqListDestroy(SqDataType* list);
+void CheckCapacity(SqList* list);
 
-void SqlistPushBack(SqList* list, SqDataType val);
-void SqlistPushFront(SqList* list, SqDataType val);
+void SqListPushBack(SqList* list, SqDataType val);
+void SqListPushFront(SqList* list, SqDataType val);
 
-void SqlistPopBack(SqList* list, SqDataType* val);
-void SqlistPopFront(SqList* list, SqDataType* val);
+void SqListPopBack(SqList* list, SqDataType* val);
+void SqListPopFront(SqList* list, SqDataType* val);
 
-void SqlistModify(SqList* list, SqDataType* val);
-void SqlistModify(SqList* list, SqDataType* val);
+void SqListInsert(SqList* list,size_t pos, SqDataType val);
+void SqListErase(SqList* list, size_t pos, SqDataType* val);
+void SqListPrint(SqList* list);
