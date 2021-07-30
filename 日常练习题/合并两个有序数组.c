aab75d41c16332471667ab026d1858acd79041b2
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-07-30 16:52:27 +0800

    Update 合并两个有序数组.c

--- 日常练习题/合并两个有序数组.c
+++ 日常练习题/合并两个有序数组.c
@@ -24,3 +24,32 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
 }
 
 
+
+//借助第三方数组
+//空间复杂度：O(M+N)
+//时间复杂度：O(M+N)
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+    
+    int n1 = 0;
+    int n2 = 0;
+
+    int *ret = (int*)malloc(sizeof(int) * (m + n));
+    int i = 0;
+
+    while(n1 < m && n2 < n)
+    {
+        if(nums1[n1] < nums2[n2])
+            ret[i++] = nums1[n1++];
+        else
+            ret[i++] = nums2[n2++];
+    }
+
+    while(n1 < m)
+        ret[i++] = nums1[n1++];
+    
+     while(n2 < n)
+        ret[i++] = nums2[n2++];
+
+    memmove(nums1,ret,sizeof(int) * (m + n));
+    free(ret);
+}
