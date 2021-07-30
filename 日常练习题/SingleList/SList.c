9fbd44ffc28e13ad9a908539ada15a8c7c36bec2
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-07-30 17:31:13 +0800

    Update SList.c

--- 日常练习题/SingleList/SList.c
+++ 日常练习题/SingleList/SList.c
@@ -1,7 +1,6 @@
 #include "SList.h"
 
-
-
+//	初始化
 void SListInit(SList* list)
 {
 	assert(list);
@@ -10,6 +9,7 @@ void SListInit(SList* list)
 	list->sz = 0;
 }
 
+//	返回一个新节点
 Node* GetNewNode()
 {
 	Node* newNode = (Node*)malloc(sizeof(Node));
@@ -17,6 +17,7 @@ Node* GetNewNode()
 	return newNode;
 }
 
+//	头插
 void SListPushFront(SList* list, SLDataType val)
 {
 	assert(list);
@@ -27,14 +28,21 @@ void SListPushFront(SList* list, SLDataType val)
 	list->head = newNode;
 }
 
+//	尾插
 void SListPushBack(SList* list, SLDataType val)
 {
-	assert(list);
-
-	Node* newNode  = GetNewNode();
+	Node* newNode = GetNewNode();
 	newNode->next = NULL;
 	newNode->val = val;
 
+	//1 当链表为空
+	if (list->head == NULL)
+	{
+		list->head = newNode;
+		return;
+	}
+
+	//2 链表不位空
 	Node* tmp = list->head;
 	while (tmp->next)
 	{
@@ -45,14 +53,122 @@ void SListPushBack(SList* list, SLDataType val)
 }
 
 
-void SListPrint(Node* node)
+//	头删
+void SListPopFront(SList* list, SLDataType* val)
 {
-	assert(node);
+	assert(list);
 
-	while (node)
-	{
-		printf("%d ->", node->val);
-		node = node->next;
-	}
-	printf("NULL");
+	//1.链表为空
+	if (list->head == NULL)
+		return;
+	//2.不为空
+	Node* next = list->head->next;
+	*val = list->head->val;
+	free(list->head);
+	list->head = next;
 }
+
+
+//	尾删
+void SListPopBack(SList* list, SLDataType* val)
+{
+	assert(list);
+
+	//1.链表为空
+	if (list->head == NULL)
+		return;
+
+	//2.链表只有一个结点
+	if (list->head->next == NULL)
+	{
+		*val = list->head->val;
+		free(list->head);
+		list->head = NULL;
+		return;
+	}
+	//3.多个结点
+	Node* prev = NULL;
+	Node* cur = list->head;
+
+	while (cur->next != NULL)
+	{
+		prev = cur;
+		cur = cur->next;
+	}
+
+	free(cur);
+	prev->next = NULL;
+}
+
+//	查找
+Node* SListFind(SList* list, SLDataType val)
+{
+	assert(list);
+
+	Node* cur = list->head;
+	while (cur)
+	{
+		if (cur->val == val)
+			return cur;
+		cur = cur->next;
+	}
+
+	return NULL;
+}
+
+
+
+void SListEraseAfter(SList* list, Node* pos)
+{
+	assert(list && pos);
+	Node* cur = list->head;
+	while (cur->next)
+	{
+		if (cur == pos)
+		{
+			Node* del = cur->next;
+			cur->next = del->next;
+			free(del);
+			break;
+		}
+		else
+		{
+			cur = cur->next;
+		}
+	}
+
+}
+
+
+
+void SListDestroy(Node* head)
+{
+	assert(head);
+
+	while (head)
+	{
+		Node* tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+}
+
+
+
+void SListPrint(Node* head)
+{
+	if (!head)
+	{
+		printf("NULL");
+		return;
+	}
+
+	while (head)
+	{
+		printf("%d ->", head->val);
+		head = head->next;
+	}
+	printf("NULL\n");
+}
+
