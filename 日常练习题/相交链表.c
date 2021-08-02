bcce1eb3b3556d081d62a10da9cd5827aa464fff
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-08-02 23:51:41 +0800

    Create 相交链表.c

---
+++ 日常练习题/相交链表.c
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+//
+typedef struct ListNode Node;
+
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    
+    if(!headA || !headB) return NULL;
+
+    Node* cur1 = headA;
+    Node* cur2 = headB;
+
+    int count1 = 0;
+    int count2 = 0;
+
+    while(cur1)
+    {
+        cur1 = cur1->next;
+        count1++;   
+    }
+
+    while(cur2)
+    {
+        cur2 = cur2->next;
+        count2++;   
+    }
+
+    cur1 = headA;
+    cur2 = headB;
+    int tmp;
+    if(count1 < count2)
+    {
+        tmp = count2 - count1;
+        while(tmp--)
+            cur2 = cur2->next;
+        
+        while(cur1 && cur2)
+        {
+            if(cur1 == cur2)
+                return cur1;
+            cur1 = cur1->next;
+            cur2 = cur2->next;
+        }
+
+        return NULL;
+    }
+    else if(count1 > count2)
+    {
+        tmp = count1 - count2;
+        while(tmp--)
+            cur1 = cur1->next;
+        
+        while(cur1 && cur2)
+        {
+            if(cur1 == cur2)
+                return cur1;
+            cur1 = cur1->next;
+            cur2 = cur2->next;
+        }
+        return NULL;
+    }
+
+      while(cur1 && cur2)
+        {
+            if(cur1 == cur2)
+                return cur1;
+            cur1 = cur1->next;
+            cur2 = cur2->next;
+        }
+    
+
+    return NULL;
+}
