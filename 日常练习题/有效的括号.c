b14e9b7230d5f4e0dc6179bb6e2fa6926d74f36d
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-08-05 23:48:48 +0800

    Create 有效的括号.c

---
+++ 日常练习题/有效的括号.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     struct Node *next;
+ *     struct Node *random;
+ * };
+ */
+
+ typedef struct Node Node;
+
+struct Node* copyRandomList(struct Node* head) {
+	
+    
+
+}
