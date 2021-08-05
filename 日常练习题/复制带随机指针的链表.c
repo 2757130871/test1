d916ac77f123fc131bebde9b1516de4d9313aaa9
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-08-05 23:47:02 +0800

    Update 复制带随机指针的链表.c

--- 日常练习题/复制带随机指针的链表.c
+++ 日常练习题/复制带随机指针的链表.c
@@ -1,16 +1,54 @@
-/**
- * Definition for a Node.
- * struct Node {
- *     int val;
- *     struct Node *next;
- *     struct Node *random;
- * };
- */
 
  typedef struct Node Node;
 
+// jjdd
+
 struct Node* copyRandomList(struct Node* head) {
-	
+
+    if(head == NULL)
+        return NULL;
+
+    Node* cur = head;
+
+    //1.链接两个链表。
+    while(cur)
+    {
+        Node* copy = (Node*)malloc(sizeof(Node));
+        copy->val = cur->val;
+        copy->next = cur->next;
+        cur->next =copy;
+
+        cur = copy->next;
+    }
+
+    cur =head;
+
+    while(cur)
+    {
+        if(cur->random)
+            cur->next->random = cur->random->next;
+        else
+            cur->next->random = NULL;
+
+        cur = cur->next->next;
+    }
     
 
+    //分割链表
+    cur = head;
+    Node* copyhead = head->next;
+
+    while(cur)
+    {
+        Node* next=  cur->next->next;
+
+        if(cur->next->next)
+            cur->next->next =next->next; 
+        else
+            cur->next->next=NULL;
+
+        cur = next;
+    }
+
+    return copyhead;
 }
